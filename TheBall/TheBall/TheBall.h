6dec0892a5aa745f43af32af0d6615037a391ba2
unknown <dima.konovko@mail.ru>
2019-10-01 01:14:45 +0300

    development upd: rebuilt ball movement logic from scratch
    Ball movement logic was updated to be more realistic: after the ball was
    hitten it has start speed and negative boost. As the result the ball's
    speed is decreasing each moment after it has been hitten
    
    mv: global variables, function declarations and user types to TheBall.h

---
+++ TheBall/TheBall/TheBall.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <windows.h>
+#include <stdlib.h>
+#include <tchar.h>
+
+
+// User types
+enum DirectionX { LEFT, RIGHT, NONE_X };
+enum DirectionY { UP, DOWN, NONE_Y };
+
+typedef struct ball {
+	float X, Y, Radius;
+	float SpeedX, SpeedY;
+	float BoostX, BoostY;
+	DirectionX directionX;
+	DirectionY directionY;
+} Ball;
+
+
+// Global variables
+static TCHAR szWindowClass[] = _T("TheBall");
+static TCHAR szTitle[] = _T("The Ball");
+constexpr auto WND_WIDTH = 1000.0;
+constexpr auto WND_HEIGHT = 600.0;
+HINSTANCE hInst;
+
+Ball ball;
+int timer = 1;
+HBITMAP hBmpBall;
+
+constexpr auto START_SPEED = 20.0;
+constexpr auto BOOST = 0.25;
+constexpr auto ALLOWED_FAULT = 1.0;
+
+// Forward declarations of functions included in this code module:
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+BOOL LeftHitten();
+BOOL RightHitten();
+BOOL TopHitten();
+BOOL BottomHitten();
+
+void LoadResources();
+void InitializeBall(HWND);
+
+void DrawBall(HWND);
+BOOL DrawBitmap(HDC hDc, int x, int y, HBITMAP hBitmap);
+
+void RecalculateBallSpeed();
+void RecalculateBallPosition();
